720a22dbfe5043a007787c8b656256b745b25f31
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2017-05-28 16:05:22 -0400

    transitionned to database data

--- app/Http/Controllers/TaxesController.php
+++ app/Http/Controllers/TaxesController.php
@@ -2,43 +2,109 @@
 
 namespace App\Http\Controllers;
 
-use App\Traits\Rates;
+use App\Rates;
+use App\Traits\Rates as RatesTrait;
 
-class TaxesController extends Controller {
-    use Rates;
+class TaxesController extends Controller
+{
+    use RatesTrait;
 
-    public function getGst() {
-        return response()->json(
-            $this->getGstRate()
-        );
+    public function getGst()
+    {
+        $rate = Rates::where('province', 'all')->orderBy('start', 'DESC')->first();
+
+        return [
+            'rate' => $rate->gst,
+            'last_modified' => $rate->updated_at->toDateString(),
+        ];
     }
 
-    public function getHst($province) {
-        $hst = $this->getHstRate($province);
-        
-        if(array_key_exists('error', $hst)){
-            return response($hst, 404);
+    public function getHst($province)
+    {
+        if ($province == "all") {
+            $rates = Rates::where('province', '!=', 'all')->get()->groupBy('province');
+            $rates = $rates->map(function ($rates, $province) {
+                $rates = $rates->sortByDesc('start');
+                $rate = $rates->first();
+
+                return [
+                    'rate' => $rate->hst,
+                    'last_modified' => $rate->updated_at->toDateString(),
+                ];
+            });
+
+            return response()->json($rates);
         }
 
-        return response()->json($hst);
+        $rate = Rates::where('province', $province)->orderBy('start', 'DESC')->first();
+        
+        if ($rate->hst == 0) {
+            return response()->json(['code' => 1000, 'message' => "There is no applicable HST in the {$province} region"]);
+        }
+        
+        return [
+            'rate' => $rate->hst,
+            'last_modified' => $rate->updated_at->toDateString(),
+        ];
     }
 
     public function getPst($province)
     {
-        $pst = $this->getPstRate($province);
 
-        if(array_key_exists('error', $pst)){
-            return response($pst, 404);
+        if ($province == "all") {
+            $rates = Rates::where('province', '!=', 'all')->get()->groupBy('province');
+            $rates = $rates->map(function ($rates, $province) {
+                $rates = $rates->sortByDesc('start');
+                $rate = $rates->first();
+
+                return [
+                    'rate' => $rate->pst,
+                    'last_modified' => $rate->updated_at->toDateString(),
+                ];
+            });
+
+            return response()->json($rates);
         }
 
-        return response()->json($pst);
+        $rate = Rates::where('province', $province)->orderBy('start', 'DESC')->first();
+
+        if ($rate->pst == 0) {
+            return response()->json(['code' => 1000, 'message' => "There is no applicable PST in the {$province} region"]);
+        }
+
+        return [
+        'rate' => $rate->pst,
+        'last_modified' => $rate->updated_at->toDateString(),
+        ];
     }
 
     public function getTotal($province)
     {
-        return response()->json(
-            $this->getTotalRate($province)
-        );
-    }
 
+        if ($province == "all") {
+            $rates = Rates::where('province', '!=', 'all')->get()->groupBy('province');
+            $rates = $rates->map(function ($rates, $province) {
+                $rates = $rates->sortByDesc('start');
+                $rate = $rates->first();
+
+                return [
+                    'rate' => $rate->applicable,
+                    'last_modified' => $rate->updated_at->toDateString(),
+                ];
+            });
+
+            return response()->json($rates);
+        }
+
+        $rate = Rates::where('province', $province)->orderBy('start', 'DESC')->first();
+
+        if ($rate->pst == 0) {
+            return response()->json(['code' => 1000, 'message' => "There is no applicable PST in the {$province} region"]);
+        }
+
+        return [
+            'rate' => $rate->applicable,
+            'last_modified' => $rate->updated_at->toDateString(),
+        ];
+    }
 }
