167d18222ba276366271306b9d8c009dedcb5671
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2017-05-28 11:40:55 -0400

    Base migration, model an controller for rates

---
+++ app/Http/Controllers/RatesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Rates;
+use Illuminate\Http\Request;
+
+class RatesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Rates  $rates
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Rates $rates)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Rates  $rates
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Rates $rates)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Rates  $rates
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Rates $rates)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Rates  $rates
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Rates $rates)
+    {
+        //
+    }
+}
