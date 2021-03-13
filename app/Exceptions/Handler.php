477102a160c2a5655510afa6b2d1ecb81f978798
Jean-Philippe Murray <jp@atomescroch.us>
2021-03-13 10:30:11 -0500

    Fix correct exception usage

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -51,10 +51,10 @@ class Handler extends ExceptionHandler
      * Convert an authentication exception into an unauthenticated response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Auth\Throwable  $exception
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
      * @return \Illuminate\Http\Response
      */
-    protected function unauthenticated($request, Throwable $exception)
+    protected function unauthenticated($request, AuthenticationException $exception)
     {
         if ($request->expectsJson()) {
             return response()->json(['error' => 'Unauthenticated.'], 401);
