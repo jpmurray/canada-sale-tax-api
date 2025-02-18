f42d6e41641e6323e999354aa1fcc26faa723c3e
Jean-Philippe Murray <jp@atomescroch.us>
2025-02-18 11:34:56 -0500

    Hidden non relevant informations in json output of a user

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,6 +41,12 @@ class User extends Authenticatable implements MustVerifyEmail
         'remember_token',
         'two_factor_recovery_codes',
         'two_factor_secret',
+        'current_team_id',
+        'profile_photo_path',
+        'is_admin',
+        'two_factor_confirmed_at',
+        'profile_photo_url',
+        'id'
     ];
 
     /**
