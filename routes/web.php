f4c009efc598d9a786646fb6fcc49d32a870598e
Jean-Philippe Murray <jp@atomescroch.us>
2025-02-18 11:12:26 -0500

    Removed default api prefix for api routes

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RateController;
-use App\Http\Controllers\API\v1\RateController as V1RateController;
-use App\Http\Controllers\API\v2\RateController as V2RateController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,37 +17,4 @@ Route::middleware([
     })->name('dashboard');
 
     Route::resource('rates', RateController::class)->except(['show', 'destroy']);
-});
-
-// old route did not have the /api prefix
-Route::prefix('/v2')->group(function () {
-    Route::group(['prefix' => '/federal'], function () {
-        Route::get('/gst/historical', [V2RateController::class, 'getHistoricalGst']);
-        Route::get('/gst/future', [V2RateController::class, 'getFutureGst']);
-        Route::get('/gst', [V2RateController::class, 'getCurrentGst']);
-    });
-
-    Route::group(['prefix' => '/province'], function () {
-        Route::get('/all', [V2RateController::class, 'getAllPst']);
-        Route::get('/{province}/historical', [V2RateController::class, 'getHistoricalPst']);
-        Route::get('/{province}/future', [V2RateController::class, 'getFuturePst']);
-        Route::get('/{province}', [V2RateController::class, 'getCurrentPst']);
-    });
-});
-
-// old route did not have the /api prefix
-Route::prefix('/v1')->group(function () {
-    Route::prefix('/federal')->group(function () {
-        Route::get('/gst', [V1RateController::class, 'getGst']);
-        Route::get('/hst/{prov}', [V1RateController::class, 'getHst']);
-        Route::get('/hst/all', [V1RateController::class, 'getHst']);
-    });
-
-    Route::group(['prefix' => '/provincial'], function () {
-        Route::get('/pst/{prov}', [V1RateController::class, 'getPst']);
-        Route::get('/pst/all', [V1RateController::class, 'getPst']);
-    });
-
-    Route::get('/total/{province}', [V1RateController::class, 'getTotal']);
-    Route::get('/total/all', [V1RateController::class, 'getTotal']);
 });
