7f181ac66e9ff309031636c74c134830a867f2e6
Jean-Philippe Murray <jp@atomescroch.us>
2025-02-27 09:19:42 -0500

    Added deprecation middleware
    Sends headers to try and notify people

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\DeprecationMiddleware;
+use App\Http\Controllers\API\v3\UserController;
 use App\Http\Controllers\API\v1\RateController as V1RateController;
 use App\Http\Controllers\API\v2\RateController as V2RateController;
 use App\Http\Controllers\API\v3\RateController as V3RateController;
-use App\Http\Controllers\API\v3\UserController;
 
 Route::prefix('/v3')->middleware(['auth:sanctum', 'throttle:api'])
     ->group(function () {
@@ -25,7 +26,7 @@ Route::prefix('/v3')->middleware(['auth:sanctum', 'throttle:api'])
         });
     });
 
-Route::prefix('/v2')->middleware(['throttle:api'])
+Route::prefix('/v2')->middleware(['throttle:api'])->middleware(DeprecationMiddleware::class . ':v2')
     ->group(function () {
         Route::group(['prefix' => '/federal'], function () {
             Route::get('/gst/historical', [V2RateController::class, 'getHistoricalGst']);
@@ -41,7 +42,7 @@ Route::prefix('/v2')->middleware(['throttle:api'])
         });
     });
 
-Route::prefix('/v1')->middleware(['throttle:api'])
+Route::prefix('/v1')->middleware(['throttle:api'])->middleware(DeprecationMiddleware::class . ':v1')
     ->group(function () {
         Route::prefix('/federal')->group(function () {
             Route::get('/gst', [V1RateController::class, 'getGst']);
