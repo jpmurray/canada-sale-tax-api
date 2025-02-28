b4f85ad54c39e4c443dec610f3430a5fbbda94af
Jean-Philippe Murray <jp@atomescroch.us>
2025-02-28 08:56:30 -0500

    Randomize HTTP 402 return
    Let's try to get people's attention in the old endpoints.

--- app/Http/Middleware/DeprecationMiddleware.php
+++ app/Http/Middleware/DeprecationMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use PhpParser\Node\Expr\Cast\String_;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -18,16 +19,18 @@ class DeprecationMiddleware
     {
         $response = $next($request);
 
-        if ($version === 'v1') {
-            $response->headers->set('X-Deprecation-Notice', 'Version 1 of the API will be deprecated on July 1st, 2025.');
-        }
+        $deprecationDate = Carbon::createFromFormat('Y-m-d', config('api.deprecation.' . $version . '.date'));
+        $sunsetDate = Carbon::createFromFormat('Y-m-d', config('api.sunset.' . $version . '.date'));
+        $message = 'Version ' . $version . ' of the API will be sunsetted on ' . $sunsetDate->toRfc7231String() . '.';
 
-        if ($version === 'v2') {
-            $response->headers->set('X-Deprecation-Notice', 'Version 2 of the API will be deprecated on December 1st, 2025.');
+        $response->headers->set('X-Deprecation-Notice', $message);
+        $response->headers->set('Deprecation', $deprecationDate->toRfc7231String());
+        $response->headers->set('Sunset', $sunsetDate->toRfc7231String());
+
+        if (mt_rand(1, 100) <= config('api.deprecation.' . $version . '.rate')) {
+            $response->setStatusCode(402); // sending subset of requests a non standard code to catch their attention
         }
 
         return $response;
-
-        // return $next($request);
     }
 }
